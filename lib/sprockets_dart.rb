180e4b29c7f0e99d7763ce994fa4907104aa073f
Michael <mmaccoffe@gmail.com>
2013-11-21 00:43:45 +0200

    Specify mime, notify about fail

--- lib/sprockets_dart.rb
+++ lib/sprockets_dart.rb
@@ -2,13 +2,18 @@ require "tilt"
 
 module Sprockets
   class DartTemplate < Tilt::Template
+    self.default_mime_type = "application/javascript"
+
     module Dart
       module Dart2Js
         def self.command; "dart2js" end
         def self.exec(input_path, output_path)
-          `#{command} --out=#{output_path} #{input_path}`
+          stdout = `#{command} --out=#{output_path} #{input_path}`
+          return stdout, $?.success?
         end
       end
+
+      class Dart2JsError < Exception; end
 
       def self.compile(content)
         dartfile = Tempfile.new("dart")
@@ -17,7 +22,12 @@ module Sprockets
         dartpath = dartfile.path
         jspath = "#{dartpath}.js"
 
-        Dart2Js.exec(dartpath, jspath)
+        msg, success = Dart2Js.exec(dartpath, jspath)
+
+        if !success
+          raise Dart2JsError, msg
+        end
+
         result = IO.read(jspath)
         result
       end
